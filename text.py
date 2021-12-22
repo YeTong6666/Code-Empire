ad35ca0496f3cd2d71fcccf23c489c5c3e76cc76
YeTong6666 <1501045805@qq.com>
2021-12-22 20:28:45 +0800

    t

--- text.py
+++ text.py
@@ -42,7 +42,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chromedriver = "/usr/bin/chromedriver"
+chromedriver = "/usr/bin/chromedriver.exe"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
 
